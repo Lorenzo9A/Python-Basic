5250258c156cde8d4bf1818054bc46dcbaa1ae29
Lorenzo Breedveld <2105144@talnet.nl>
2020-11-10 10:44:39 +0100

    af

---
+++ 2-vervolg datatypes/taak02 -dicts/dicts.py
@@ -0,0 +1,18 @@
+Provinciehoofdsteden = dict(
+Noord_Holland='Amsterdam',
+Zuid_Holland='Den_Haag',
+Utrecht='Utrecht',
+)
+print(Provinciehoofdsteden)
+
+
+Dagen_in_maanden = dict([
+('januari', 31),
+('februari',28),
+('maart',31)
+])
+print(Dagen_in_maanden)
+Provinciehoofdsteden['Zeeland'] = 'Middelburg'
+Provinciehoofdsteden['Limburg'] = 'Maastricht'
+Provinciehoofdsteden['Gelderland'] = 'Arnhem'
+print(Provinciehoofdsteden)
