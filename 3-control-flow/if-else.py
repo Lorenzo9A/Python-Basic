7ad8b97b17639622948dd93ee41805072ab3dfc1
Lorenzo Breedveld <2105144@talnet.nl>
2020-11-10 11:28:10 +0100

    af

---
+++ 3-control-flow/if-else.py
@@ -0,0 +1,15 @@
+
+a = 5
+if a == 5:
+  print("a is gelijk aan 5")
+else:
+  print("a is niet gelijk aan 5")
+
+
+a = 5
+if a == 5:
+  print("a is gelijk aan 5")
+elif a == 0:
+  print("a is gelijk aan 0")
+else:
+  print("a is niet gelijk aan beide")
